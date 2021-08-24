6bf1179f553146b94afa45bed892ec0607436795
najm <jemmeli84@gmail.com>
2021-08-24 23:57:19 +0100

    string interpolation and event-propoerty one way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstApp';
+
+  nom = 'Boubakri';
+  prenom = 'Seif';
+  email = 'saif@gmail.com';
+  DEFAULT = 'saif@gmail.com';
+
+  change( data:any ){
+    //console.log( data );
+    this.email = data;
+  }
+
+  reset(){
+    this.email = this.DEFAULT;
+  }
+
 }
